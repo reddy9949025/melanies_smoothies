297ea2ac674d1e782361e4a6c07c5e51c82db6cc
reddy9949025 <purushotham.m.reddy@kipi.ai>
2025-08-04 17:09:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
-st.title("My Custom Smoothie!:🥤")
+st.title("My Custom Smoothie!:")
 st.write(
   """choose the fruits you want in your custom smoothie!
   """
@@ -19,17 +19,15 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 ingredients_list=st.multiselect('Choose up to 5 ingredients:',my_dataframe, max_selections=5)
 
 
-if ingredients_list:
-    ingredients_string= ''
-    for fruit_chosen in ingredients_list:
-        ingredients_string+=fruit_chosen + ' '
-
-    my_insert_stmt = "insert into smoothies.public.fruit_options(ingredients) values ('" + ingredients_string + "', '" + name_on_order + "')"
-
-    time_to_insert=st.button("submit order")
-
-    if ingredients_string:
+if ingredient_list:
+    ingredient_string = ''
+    for fruit_chosen in ingredient_list:
+            ingredient_string += fruit_chosen + ' '
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
+        values ('""" + ingredient_string + """')"""
+    time_to_insert = st.button('Submit Order')
+    if time_to_insert:
         session.sql(my_insert_stmt).collect()
-        st.success('your smoothie is ordered!')
+        st.success('Your Smoothie is ordered!')
 
 
