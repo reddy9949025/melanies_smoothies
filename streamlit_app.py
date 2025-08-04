deb97dcc0a887937f2c311a132b1e3d16bff474d
reddy9949025 <purushotham.m.reddy@kipi.ai>
2025-08-04 17:28:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,6 +28,6 @@ if ingredient_list:
     time_to_insert = st.button('Submit Order')
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
-        st.success('Your Smoothie is ordered!')
+        st.success('Your Smoothie is ordered, {name_on_order}!')
 
 
