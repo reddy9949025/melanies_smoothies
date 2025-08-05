44387344633c042de3c5da834b0e6880f8ae221a
reddy9949025 <purushotham.m.reddy@kipi.ai>
2025-08-05 10:13:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-import requests
 
 # Write directly to the app
 st.title("My Custom Smoothie!:")
@@ -18,7 +17,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 #st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
 
-pd_df=my_dataframe.to_pandas()
+#pd_df=my_dataframe.to_pandas()
 #st.dataframe(pd_df)
 #st.stop()
 
@@ -30,14 +29,17 @@ if ingredient_list:
     ingredient_string = ''
     for fruit_chosen in ingredient_list:
             ingredient_string += fruit_chosen + ' '
-            search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+            #search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
             #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
-            st.subheader(fruit_chosen+'Nutrition Information')
-            smoothiefroot_response = requests.get("https://smoothiefroot.com/api/fruit/"+ search_on)
-            sf_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+            #st.subheader(fruit_chosen+'Nutrition Information')
+            #smoothiefroot_response = requests.get("https://smoothiefroot.com/api/fruit/"+ search_on)
+            #sf_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
         values ('""" + ingredient_string + """')"""
     time_to_insert = st.button('Submit Order')
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success(f"Your Smoothie is ordered, {name_on_order}!")
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
